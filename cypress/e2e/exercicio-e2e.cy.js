bcb4e1b152a4f8004a9922da9f4578b9d18aeba7
Fabiano <fabianodiogo8545@gmail.com>
2025-07-14 11:59:55 -0300

    Atividade teste

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,6 +1,9 @@
 //// <reference types="cypress" />
 import ProdutoPage from '../support/page_objects/nome-funcionliada.page.js';
 
+let dadosLogin;
+let usuario = 'aluno_ebac@teste.com';
+let senha = 'teste@teste.com';
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -12,39 +15,58 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
   beforeEach(() => {
     ProdutoPage.visitarUrl();
+  
+
   });
 
-    it('Deve buscar Produtos lista com sucesso', () => {
-        ProdutoPage.buscarProdutolista('Abominable Hoodie' );
-           cy.get('.product_title').should('contain', 'Abominable Hoodie');
+       it('Deve buscar Produtos com sucesso', () => {
+       
+         ProdutoPage.buscarProduto('Erica Evercool Sports Bra');
+           cy .get('.product_title').should('contain', 'Erica Evercool Sports Bra');
+ 
+             ProdutoPage.buscarProduto('Autumn Pullie');
+              cy .get('.product_title').should('contain', 'Autumn Pullie');
+
+             ProdutoPage.buscarProduto('Bruno Compete Hoodie');
+              cy .get('.product_title').should('contain', 'Bruno Compete Hoodie');
+
+               ProdutoPage.buscarProduto('Eos V-Neck Hoodie');
+                cy .get('.product_title').should('contain', 'Eos V-Neck Hoodie');
     });
+      it('Deve adicionar produtos ao carrinho', () => {
+        ProdutoPage.buscarProduto('Erica Evercool Sports Bra');
+          ProdutoPage.addicionarProdutoCarrinho('M', 'Blue', 1);
+            cy.get('.woocommerce-message').should('contain', 'Erica Evercool Sports Bra” foi adicionado no seu carrinho.');
+            
+              ProdutoPage.buscarProduto('Autumn Pullie');
+                ProdutoPage.addicionarProdutoCarrinho('M', 'Red', 1);
+                 cy.get('.woocommerce-message').should('contain', 'Autumn Pullie” foi adicionado no seu carrinho.');
 
-        it('Deve buscar Produtos lista com sucesso', () => {
-              ProdutoPage.buscarProdutolista('Apollo Running Short');
-                 cy.get('.product_title').should('contain', 'Apollo Running Short');
-        });
-          it('Deve buscar Produtos lista com sucesso', () => {
-                ProdutoPage.buscarProdutolista('Aero Daily Fitness Tee');
-                     cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee');
-          })
-            it('Deve buscar Produtos lista com sucesso', () => {
-                    ProdutoPage.buscarProdutolista('Ariel Roll Sleeve Sweatshirt');
-                         cy.get('.product_title').should('contain', 'Ariel Roll Sleeve Sweatshirt');
-            });
-                 it('Deve buscar Produtos lista com sucesso', () => {
-                    ProdutoPage.buscarProdutolista('Ariel Roll Sleeve Sweatshirt');
-                         cy.get('.product_title').should('contain', 'Ariel Roll Sleeve Sweatshirt');
-            });
-  
-  
-  it('Deve adicionar produtos ao carrinho', () => {
+                   ProdutoPage.buscarProduto('Bruno Compete Hoodie');
+                    ProdutoPage.addicionarProdutoCarrinho('M', 'Black', 1);
+                     cy.get('.woocommerce-message').should('contain', '“Bruno Compete Hoodie” foi adicionado no seu carrinho.');
+
+                       ProdutoPage.buscarProduto('Eos V-Neck Hoodie');
+                        ProdutoPage.addicionarProdutoCarrinho('M', 'Blue', 1);
+                         cy.get('.woocommerce-message').should('contain', 'Eos V-Neck Hoodie” foi adicionado no seu carrinho.');
+          
+        cy.get('.woocommerce-message > .button').click();
+          cy.get('.checkout-button').click();
+           cy.get('.showlogin').click();
+            cy.get('#username').type(usuario); 
+             cy.get('#password').type(senha)
+              cy.get('.woocommerce-button').click();
+               cy.get('#terms').click();
+                cy.get('#place_order').click();
+                 cy.get('.woocommerce-notice',{timeout:8000}).should('contain', 'Obrigado. Seu pedido foi recebido.');
+                  
+               
+      })
+
+           
 
 
-  })
-    it('Deve preencher todas as opções no checkout', () => {
 
-})
-    it('Deve validar a compra ao final', () => {
-    
-    })
-    })
+
+});
+
--- cypress/support/page_objects/nome-funcionliada.page.js
+++ cypress/support/page_objects/nome-funcionliada.page.js
@@ -3,7 +3,9 @@ class ProdutoPage {
    visitarUrl() {
     cy.visit('produtos');
     }
-     buscarProduto() {
+     buscarProduto(nomeProduto) {
+        cy.get('[name="s"]').eq(1).type(nomeProduto);
+        cy.get('.button-search').eq(1).click();
 
 }
     buscarProdutolista(nomeProduto) {
@@ -15,8 +17,11 @@ class ProdutoPage {
     visitarProduto() {
         
     }
-    addicionarProdutoCarrinho() {
-
+    addicionarProdutoCarrinho(tamanho, cor, quantidade) {
+      cy.get('.button-variable-item-'+ tamanho).click();
+      cy.get('.button-variable-item-'+ cor).click();
+      cy.get('.input-text').clear().type(quantidade);
+      cy.get('.single_add_to_cart_button').click();
     }
 }
 export default new ProdutoPage()
